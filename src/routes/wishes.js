5b4f250f3f6591f64bd1102fba4c504d5be1a28e
Fernando Carmona <fernando.carmona@gruposantander.com>
2021-01-11 21:30:55 +0100

    step 7

--- src/routes/wishes.js
+++ src/routes/wishes.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { read, write, createError } = require('../utils');
 
 const wishesDbPath = path.join(__dirname, '../db/wishes.json');
+const scoresDbPath = path.join(__dirname, '../db/scores.json')
 
 const router = express.Router();
 
@@ -10,9 +11,15 @@ router.get('/', async (req, res, next) => {
   try {
     const { present } = req.query;
     const wishes = await read(wishesDbPath);
+    const scores = await read(scoresDbPath);
+
+    const wishesWithScore = wishes.reduce((acc, next) => {
+      const score = scores.filter(score => score.name === next.name)[0].score
+      return [...acc, {...next, score}]
+    }, [])
 
     if (present) {
-      const filteredWishes = wishes.filter((wish) =>
+      const filteredWishes = wishesWithScore.filter((wish) =>
         wish.presents.includes(present) ? wish : false
       );
       if (!filteredWishes.length > 0) {
@@ -28,7 +35,7 @@ router.get('/', async (req, res, next) => {
 
     res.status(200).json({
       data: {
-        wishes,
+        wishesWithScore,
       },
       status: 'ok',
     });
@@ -41,7 +48,14 @@ router.get('/:name', async (req, res, next) => {
   try {
     const name = req.params.name.toLowerCase();
     const wishes = await read(wishesDbPath);
-    const unique = wishes.filter((wish) => wish.name.toLowerCase() === name);
+    const scores = await read(scoresDbPath);
+
+    const wishesWithScore = wishes.reduce((acc, next) => {
+      const score = scores.filter(score => score.name === next.name)[0].score
+      return [...acc, {...next, score}]
+    }, [])
+
+    const unique = wishesWithScore.filter((wish) => wish.name.toLowerCase() === name);
     if (!unique.length > 0) {
       createError('No kid with that name', 404);
     }
