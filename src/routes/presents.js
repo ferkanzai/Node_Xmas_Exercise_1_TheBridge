c9b76e2bb7155a61166d95405193214e66758785
Fernando Carmona <fernando.carmona@gruposantander.com>
2021-01-11 22:51:12 +0100

    stared step 9 - not finished

--- src/routes/presents.js
+++ src/routes/presents.js
@@ -3,6 +3,8 @@ const path = require('path');
 const { read, write, createError } = require('../utils');
 
 const wishesDbPath = path.join(__dirname, '../db/wishes.json');
+const presentsDbPath = path.join(__dirname, '../db/presents.json');
+const scoresDbPath = path.join(__dirname, '../db/scores.json');
 
 const router = express.Router();
 
@@ -18,6 +20,43 @@ router.post('/', async (req, res, next) => {
 
     res.status(200).json({
       data: 'file written',
+      status: 'ok',
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/:name', async (req, res, next) => {
+  try {
+    const name = req.params.name.toLowerCase();
+    const presents = await read(presentsDbPath);
+    const presentsKid = await read(presentsDbPath);
+    const presentFiltered = presentsKid.filter((kid) => kid.name.toLowerCase() === name);
+    const wishesKid = await read(wishesDbPath);
+    const wishesFiltered = wishesKid.filter((kid) => kid.name.toLowerCase() === name);
+    const scoresKid = await read(scoresDbPath);
+    const scoresFiltered = scoresKid.filter((kid) => kid.name.toLowerCase() === name);
+
+    console.log(presentFiltered);
+    console.log(wishesFiltered);
+    console.log(scoresFiltered);
+
+    if (scoresKid.score > 7) {
+      console.log(true)
+      const toWrite = presents.map((present) => {
+        if (present.name.toLowerCase() === name) {
+          present.presents = wishesFiltered.presents;
+        }
+        return present;
+      });
+      await write(presentsDbPath, toWrite);
+    }
+
+    res.status(200).json({
+      data: {
+        toWrite,
+      },
       status: 'ok',
     });
   } catch (error) {
